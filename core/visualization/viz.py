6f925631cefd2f8e798d2b731b95493e741dbe77
Suzie14 <suzie.grondin@gmail.com>
2023-10-30 10:43:14 -0400

    minor changes

--- core/visualization/viz.py
+++ core/visualization/viz.py
@@ -1,15 +1,13 @@
 import matplotlib as plt
 import seaborn as sns 
 
-def plot_learning_curve(mean_df):
+def plot_learning_curve(mean_df, nb_iter):
     # Assuming df contains 'Rewards' and 'Iteration' columns
     plt.figure(figsize=(10, 6))
 
     # Plot rewards for each player
-    plt.plot(mean_df['Iteration'], mean_df['Reward1'],
-             alpha=0.5, label='Player 1')
-    plt.plot(mean_df['Iteration'], mean_df['Reward2'],
-             alpha=0.5, label='Player 2')
+    plt.plot(mean_df['Iteration'][mean_df['Iteration']<nb_iter], mean_df['Reward1'][mean_df['Iteration']<nb_iter],alpha=0.5, label='Player 1')
+    plt.plot(mean_df['Iteration'][mean_df['Iteration']<nb_iter], mean_df['Reward2'][mean_df['Iteration']<nb_iter],alpha=0.5, label='Player 2')
 
     # Customize the plot
     plt.title('Learning Curves')
