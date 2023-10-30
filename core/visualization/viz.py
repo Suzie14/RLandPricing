56cef6572426a40b574bfcd577542ef5aae04bbb
Suzie14 <suzie.grondin@gmail.com>
2023-10-30 10:29:44 -0400

    save modifs

--- core/visualization/viz.py
+++ core/visualization/viz.py
@@ -1,5 +1,5 @@
 import matplotlib as plt
-
+import seaborn as sns 
 
 def plot_learning_curve(mean_df):
     # Assuming df contains 'Rewards' and 'Iteration' columns
@@ -17,4 +17,27 @@ def plot_learning_curve(mean_df):
     plt.ylabel('Rewards')
     plt.legend()
     plt.grid(True)
+    plt.show()
+
+def repart_rewards(df_beta):
+    sns.set(style="whitegrid")
+    plt.figure(figsize=(8, 6))
+    
+    # Sort the dataframe by 'beta' column in ascending order
+    df_beta_sorted = df_beta.sort_values(by='beta')
+
+    
+    sns.boxplot(x='beta', y=df_beta_sorted['Reward1'], data=df_beta_sorted, fill=False, order=df_beta_sorted['beta'])
+    sns.boxplot(x='beta', y=df_beta_sorted['Reward2'], data=df_beta_sorted, fill=False, order=df_beta_sorted['beta'])
+    
+    plt.xlabel('Beta')
+    plt.ylabel('Final rewards')
+    plt.title('Box Plot of final rewards grouped by $\\beta$')
+    
+    # Define custom tick labels for the x-axis
+    beta_values = df_beta_sorted['beta'].unique()
+    beta_labels = ['{:.1e}'.format(beta) for beta in beta_values]
+    
+    # Set custom tick positions and labels on the x-axis
+    plt.xticks(ticks=range(len(beta_values)), labels=beta_labels, rotation=45)
     plt.show()
