1bc0eae917dd8f6fcfcaf3484e5ba1cb72f5004a
Suzie14 <suzie.grondin@gmail.com>
2023-11-02 22:43:14 -0400

    new job diff beta

---
+++ job_scripts/job_diff_epsilon.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --mem=16G
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --time=10:00:0    
+#SBATCH --mail-user=grondin.suzie@courrier.uqam.ca
+#SBATCH --mail-type=ALL
+
+cd ~/$project
+module purge
+module load python/3.9.0 scipy-stack
+source ~/venvtest/bin/activate
+
+python cluster/efficency_diff_beta.py
